59c43e74eabf6678be21caec048bbe1c4366d1d3
Danilo Pootaren <danilo.pootaren@codeworldwide.com>
2022-05-09 13:59:39 +0400

    dockerize app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
